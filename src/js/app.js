af7029d52b15be56ac4b4a42937a3180a94eef1b
Gosia <malgorzata.krysztofik@gmail.com>
2020-06-11 14:23:43 +0200

    Add functionality of subpages handling

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,77 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+ 
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    // thisApp.activatePage(thisApp.pages[0].id);
+    // jeśli zmienię indeks na 1, defaultowo odpali się podstrona booking
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        // get page id from href attribute
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        // run thisApp.activatePage with that id
+        thisApp.activatePage(id);
+
+        // change URL hash
+        window.location.hash = '#/' + id;
+        //do adresu strony zostanie dodana koncowka order lub booking, ale strona tez sie przewinela na dol
+        // dopoki nie dodasz slasha
+
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    // add class "active" to matching pages, remove from non-matching
+
+    for(let page of thisApp.pages){
+      // if(page.id == pageId){
+      //   page.classList.add(classNames.pages.active);
+      // } else {
+      //   page.classList.remove(classNames.pages.active);
+      // } pod spodem to samo w innym zapisie
+
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+
+    }
+
+    // add class "active" to matching links, remove from non-matching
+
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+
+    }
+  }, 
+
   initMenu: function () {
     const thisApp = this;
 
@@ -37,6 +106,7 @@ const app = {
   },
   init: function () {
     const thisApp = this;
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
